3eaebd5d85ea9d1f07f02c048b174591babf1bce
mehedi hasan <mehedicsebu@gmail.com>
2022-02-06 12:23:44 +0600

    aray output printed done

---
+++ for_of_loop.js
@@ -0,0 +1,16 @@
+// getting  out put from an Array
+
+let Array1 = [12, 43, 56, 77];
+let Array2 = ['mehdi', 'hasan', 'rubel'];
+
+// read array1 value
+for (const value_of_array_1 of Array1) {
+    console.log(value_of_array_1);
+}
+
+// read array2 value
+for (const value_of_array_2 of Array2) {
+    console.log(value_of_array_2);
+}
+
+// printing value with array also
