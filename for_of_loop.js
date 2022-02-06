78d352fcec8a7d2ebdff599325201e9c26d7547f
mehedi hasan <mehedicsebu@gmail.com>
2022-02-06 12:35:48 +0600

    output from array index

--- for_of_loop.js
+++ for_of_loop.js
@@ -1,4 +1,5 @@
 // getting  out put from an Array
+console.log('getting  out put from an Array');
 
 let Array1 = [12, 43, 56, 77];
 let Array2 = ['mehdi', 'hasan', 'rubel'];
@@ -14,3 +15,14 @@ for (const value_of_array_2 of Array2) {
 }
 
 // printing value with array also
+console.log('printing value with array also');
+for (const [indexNum, value] of Array1.entries()) {
+    console.log(indexNum, value);
+}
+for (const [indexNum, value] of Array2.entries()) {
+    console.log(indexNum, value);
+}
+
+//printing value of selected array
+const selectedIndexValue = Array1[0];
+console.log('Selected index value is: ', selectedIndexValue);
